34a7558196853fbde23115e90ece1ca2199b3094
Sebi Lopez <36292214+Sebi-Lopez@users.noreply.github.com>
2020-02-11 10:09:17 +0100

    Pushed files created in class

---
+++ AR/CV/test.py
@@ -0,0 +1,41 @@
+print("Hello darkness")
+
+gerard = True
+gerardNom = "Clotet"
+gerardNumber = 100
+gerardNomfromNumber = format(54)
+print(gerard)
+print(gerardNom)
+print(gerardNumber)
+print(gerardNomfromNumber)
+
+numberINPT = input("Introduce number: ")
+print(numberINPT)
+
+number = 100
+
+if number > 100:
+    print("El gerard es grandote")
+elif number < 100:
+    print("El gerard no en te ni idea")
+else: 
+    print("100 es 100")
+
+
+running = True
+
+while running:
+    number+=1
+    print(number)
+
+    if number >= 500:
+        running = False
+    
+else: 
+    print("Loop is over")
+
+for i in range(5,20):
+    print(i)
+
+else: print("For loop is over")
+
